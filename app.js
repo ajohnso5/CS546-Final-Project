ce62fc8a1f4fef9137f7615463b04e8197151a6b
ccousill <43454787+ccousill@users.noreply.github.com>
2020-12-07 20:14:41 -0500

    Add files via upload

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const session = require('express-session');
 const configRoutes = require('./routes');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 app.use(express.json());
 
 
@@ -30,8 +30,6 @@ app.use('/', (req,res,next) => {
     else{
        next();
     }
-
-   
 });
 
 app.use('/dashboard', (req, res, next) => {
